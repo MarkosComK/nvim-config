8dc964474f9eba3db09f2098a1898e6aa3b0fc17
MarkosComK <markoscomks@gmail.com>
2024-10-19 12:02:44 +0100

    Newer config

---
+++ init.lua
@@ -0,0 +1,56 @@
+require('settings')
+
+-- Configuração do lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- versão estável
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Carregar plugins com lazy.nvim
+require("lazy").setup({
+	  {
+    'echasnovski/mini.icons',
+    event = 'VimEnter',  -- or any appropriate event
+  },
+	require 'plugins.telescope',
+	  -- Outros plugins...
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp", -- Adicione este plugin
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup {
+        mapping = {
+          -- Configure suas teclas de atalho
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+        },
+      }
+    end,
+  },
+	require 'plugins.neo-tree',
+	require 'plugins.coc',
+	require 'plugins.lsp',
+	require 'plugins.auto-pairs',
+	require 'plugins.web-devicons',
+	require 'plugins.42-header',
+	require 'plugins.42-norminette',
+	require 'plugins.barbar',
+	require 'themes.cyberdream',
+	-- Theme setup
+})
---
+++ lua/settings.lua
@@ -0,0 +1,46 @@
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Mostrar números de linha e números relativos
+vim.opt.number = true   -- Exibe números de linha
+vim.opt.relativenumber = true  -- Exibe números relativos
+
+-- Configurar tamanho de tabulação e recuo
+vim.opt.tabstop = 4       -- Define tabulações para 4 espaços
+vim.opt.shiftwidth = 4    -- Define o recuo automático para 4 espaços
+vim.opt.expandtab = false -- keep tabulações
+vim.g.have_nerd_font = true
+
+-- Configurar navegação entre janelas com Ctrl + hjkl
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true }) -- Janela à esquerda
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true }) -- Janela abaixo
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true }) -- Janela acima
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true }) -- Janela à direita
+
+-- Mapeamentos para navegação em modo de inserção
+vim.api.nvim_set_keymap('i', '<C-h>', '<C-o><C-w>h', { noremap = true, silent = true }) -- Janela à esquerda
+vim.api.nvim_set_keymap('i', '<C-j>', '<C-o><C-w>j', { noremap = true, silent = true }) -- Janela abaixo
+vim.api.nvim_set_keymap('i', '<C-k>', '<C-o><C-w>k', { noremap = true, silent = true }) -- Janela acima
+vim.api.nvim_set_keymap('i', '<C-l>', '<C-o><C-w>l', { noremap = true, silent = true }) -- Janela à direita
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- reserve a column to compilation erros
+vim.opt.signcolumn = "yes"
+
+-- Navigation into buffers /barbar plugin
+vim.api.nvim_set_keymap('n', 'H', '<Cmd>BufferPrevious<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'L', '<Cmd>BufferNext<CR>', { noremap = true, silent = true })
+-- Key mapping to close the current buffer/tab in barbar.nvim
+vim.api.nvim_set_keymap('n', '<C-q>', '<Cmd>BufferClose<CR>', { noremap = true, silent = true })
+
+--prevent swap files
+vim.opt.swapfile = false
