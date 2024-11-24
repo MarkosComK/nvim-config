426b34ef480c3de82e76ad17fe41f937b0677051
MarkosComK <markoscomks@gmail.com>
2024-11-24 12:08:06 +0000

    Set new identation in python to be tabs instead of spaces

--- lua/settings.lua
+++ lua/settings.lua
@@ -44,3 +44,12 @@ vim.api.nvim_set_keymap('n', '<C-q>', '<Cmd>BufferClose<CR>', { noremap = true, 
 
 --prevent swap files
 vim.opt.swapfile = false
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  callback = function()
+    vim.opt_local.expandtab = false
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+  end,
+})
