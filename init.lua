0be208e13859f66ff1f705be6eedfa0ab90c3264
MarkosComK <markoscomks@gmail.com>
2024-11-27 20:04:24 +0000

    Added flutter support

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 require('settings')
-
 -- Configuração do lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -16,33 +15,71 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Carregar plugins com lazy.nvim
 require("lazy").setup({
-	  {
-    'echasnovski/mini.icons',
-    event = 'VimEnter',  -- or any appropriate event
-  },
+	{
+	"nvim-java/nvim-java",
+	config = function()
+		require("java").setup() -- Configuração básica
+		require("lspconfig").jdtls.setup({}) -- Configuração do LSP
+	end,
+	},
+	{
+		"OXY2DEV/markview.nvim",
+		lazy = false,      -- Recommended
+		-- ft = "markdown" -- If you decide to lazy-load anyway
+
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons"
+		}
+	},
+	{
+		"akinsho/flutter-tools.nvim",
+		lazy = false,
+		dependencies = {
+		  "nvim-lua/plenary.nvim",
+		  "stevearc/dressing.nvim",
+		},
+		config = function()
+			require("flutter-tools").setup({
+			lsp = {
+			  color = {
+				enabled = true,
+			  },
+			  settings = {
+				showTodos = true,
+				completeFunctionCalls = true,
+			  }
+			}
+			})
+		end
+	},
+	{
+		'echasnovski/mini.icons',
+		event = 'VimEnter',  -- or any appropriate event
+	},
 	require 'plugins.telescope',
-	  -- Outros plugins...
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp", -- Adicione este plugin
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-    },
-    config = function()
-      local cmp = require("cmp")
-      cmp.setup {
-        mapping = {
-          -- Configure suas teclas de atalho
-        },
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "buffer" },
-        },
-      }
-    end,
-  },
+		  -- Outros plugins...
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+		  "hrsh7th/cmp-nvim-lsp", -- Adicione este plugin
+		  "hrsh7th/cmp-buffer",
+		  "hrsh7th/cmp-path",
+		  "hrsh7th/cmp-cmdline",
+		},
+		config = function()
+		local cmp = require("cmp")
+		cmp.setup {
+			mapping = {
+			  -- Configure suas teclas de atalho
+			},
+			sources = {
+			  { name = "nvim_lsp" },
+			  { name = "buffer" },
+			},
+			}
+		end,
+	},
 	require 'plugins.neo-tree',
 	require 'plugins.coc',
 	require 'plugins.lsp',
