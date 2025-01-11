9ccdfe1a143bc262d27a48f091daacf90c78b331
MarkosComK <markoscomks@gmail.com>
2025-01-11 11:49:38 +0000

    Added transparency for sidebar

--- lua/themes/melange.lua
+++ lua/themes/melange.lua
@@ -1,19 +1,22 @@
 return {
-  "savq/melange-nvim",
-  config = function()
-    vim.cmd("colorscheme melange") -- Activate Melange colorscheme
-    vim.opt.background = "dark"    -- Set dark background mode
-
-    -- Enable transparency by clearing background colors
-    vim.cmd([[
-      hi Normal guibg=NONE ctermbg=NONE
-      hi NormalNC guibg=NONE ctermbg=NONE
-      hi LineNr guibg=NONE ctermbg=NONE
-      hi SignColumn guibg=NONE ctermbg=NONE
-      hi VertSplit guibg=NONE ctermbg=NONE
-      hi StatusLine guibg=NONE ctermbg=NONE
-      hi StatusLineNC guibg=NONE ctermbg=NONE
-      hi EndOfBuffer guibg=NONE ctermbg=NONE
-    ]])
-  end
+	"savq/melange-nvim",
+	config = function()
+		vim.cmd("colorscheme melange") -- Activate Melange colorscheme
+		vim.opt.background = "dark"    -- Set dark background mode
+		-- Enable transparency by clearing background colors
+		vim.cmd([[
+			hi Normal guibg=NONE ctermbg=NONE
+			hi NormalNC guibg=NONE ctermbg=NONE
+			hi LineNr guibg=NONE ctermbg=NONE
+			hi SignColumn guibg=NONE ctermbg=NONE
+			hi VertSplit guibg=NONE ctermbg=NONE
+			hi StatusLine guibg=NONE ctermbg=NONE
+			hi StatusLineNC guibg=NONE ctermbg=NONE
+			hi EndOfBuffer guibg=NONE ctermbg=NONE
+			" Transparency for Neotree sidebar
+			hi NeotreeNormal guibg=NONE ctermbg=NONE
+			hi NeotreeIndentMarker guibg=NONE ctermbg=NONE
+			hi NeotreeTab guibg=NONE ctermbg=NONE
+		]])
+	end
 }
