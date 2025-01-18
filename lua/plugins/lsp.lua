fae2de525b95cbd9d9a36643217c7dc28f9efe05
Markos Santos <msantos@devexperts.com>
2025-01-18 20:31:45 +0000

    idk

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,120 +1,132 @@
 return {
-  'neovim/nvim-lspconfig',
-  dependencies = {
-    {
-      'williamboman/mason.nvim',
-      config = true,
-      opts = {
-        ensure_installed = {
-          "pyright",
-          "rust-analyzer",
-        },
-      },
-    },
-    'williamboman/mason-lspconfig.nvim',
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
-    { 'j-hui/fidget.nvim', opts = {} },
-    { 'folke/neodev.nvim', opts = {} },
-  },
-  config = function()
-    -- LSP attach function
-    vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-      callback = function(event)
-        local map = function(keys, func, desc)
-          vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-        end
+	'neovim/nvim-lspconfig',
+	dependencies = {
+		{
+			'williamboman/mason.nvim',
+			config = true,
+			opts = {
+				ensure_installed = {
+					"pyright",
+					"rust-analyzer",
+					"zls",
+				},
+			},
+		},
+		'williamboman/mason-lspconfig.nvim',
+		'WhoIsSethDaniel/mason-tool-installer.nvim',
+		{ 'j-hui/fidget.nvim', opts = {} },
+		{ 'folke/neodev.nvim', opts = {} },
+	},
+	config = function()
+	-- LSP attach function
+		vim.api.nvim_create_autocmd('LspAttach', {
+			group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+			callback = function(event)
+			local map = function(keys, func, desc)
+			  vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+			end
 
-        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-        map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-        map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-        map('K', vim.lsp.buf.hover, 'Hover Documentation')
-        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+			map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+			map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+			map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+			map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+			map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+			map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+			map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+			map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+			map('K', vim.lsp.buf.hover, 'Hover Documentation')
+			map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-        -- Document highlight
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client.server_capabilities.documentHighlightProvider then
-          local highlight_group = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-            buffer = event.buf,
-            group = highlight_group,
-            callback = vim.lsp.buf.document_highlight,
-          })
-          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-            buffer = event.buf,
-            group = highlight_group,
-            callback = vim.lsp.buf.clear_references,
-          })
-        end
-      end,
-    })
+			-- Document highlight
+			local client = vim.lsp.get_client_by_id(event.data.client_id)
+			if client and client.server_capabilities.documentHighlightProvider then
+				local highlight_group = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+				vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+					buffer = event.buf,
+					group = highlight_group,
+					callback = vim.lsp.buf.document_highlight,
+				})
+				vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+					buffer = event.buf,
+					group = highlight_group,
+					callback = vim.lsp.buf.clear_references,
+				})
+			end
+		end,
+		})
 
-    -- Server capabilities
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+		-- Server capabilities
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-    -- Server configurations
-    local servers = {
-      clangd = {},
-      pyright = {},
-      html = {},
-      cssls = {},
-      tsserver = {},
-	  rust_analyzer = {},
-      emmet_ls = {
-        filetypes = { 
-          'html', 
-          'css', 
-          'scss', 
-          'javascript', 
-          'javascriptreact',
-          'typescript', 
-          'typescriptreact'
-        },
-      },
-      lua_ls = {
-        settings = {
-          Lua = {
-            completion = { callSnippet = 'Replace' },
-          },
-        },
-      },
-    }
+		-- Server configurations
+		local servers = {
+			clangd = {},
+			pyright = {},
+			html = {},
+			cssls = {},
+			tsserver = {},
+			rust_analyzer = {},
+			zls = {
+				capabilities = {
+					textDocument = {
+						completion = {
+							completionItem = {
+								snippetSupport = true
+							}
+						}
+					}
+				}
+			},
+			emmet_ls = {
+				filetypes = {
+					'html',
+					'css',
+					'scss',
+					'javascript',
+					'javascriptreact',
+					'typescript',
+					'typescriptreact'
+				},
+			},
+			lua_ls = {
+				settings = {
+					Lua = {
+						completion = { callSnippet = 'Replace' },
+					},
+				},
+			},
+		}
 
-    require('mason').setup()
-    local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      'stylua',
-      'prettier',
-    })
-    
-    require('mason-lspconfig').setup {
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          require('lspconfig')[server_name].setup(server)
-        end,
-      },
-    }
+		require('mason').setup()
+		local ensure_installed = vim.tbl_keys(servers or {})
+		vim.list_extend(ensure_installed, {
+			'stylua',
+			'prettier',
+		})
+		
+		require('mason-lspconfig').setup {
+			handlers = {
+				function(server_name)
+				  local server = servers[server_name] or {}
+				  server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+				  require('lspconfig')[server_name].setup(server)
+				end,
+			},
+		}
 
-    -- Web files tab configuration
-    local web_filetypes = { "html", "css", "scss", "javascript", "typescript", "javascriptreact", "typescriptreact" }
-    for _, ft in ipairs(web_filetypes) do
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = ft,
-        callback = function()
-          vim.opt_local.expandtab = false
-          vim.opt_local.tabstop = 4
-          vim.opt_local.shiftwidth = 4
-          vim.opt_local.softtabstop = 4
-        end
-      })
-    end
-  end,
+		-- Web files tab configuration
+		local web_filetypes = { "html", "css", "scss", "javascript", "typescript", "javascriptreact", "typescriptreact" }
+		for _, ft in ipairs(web_filetypes) do
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = ft,
+				callback = function()
+					vim.opt_local.expandtab = false
+					vim.opt_local.tabstop = 4
+					vim.opt_local.shiftwidth = 4
+					vim.opt_local.softtabstop = 4
+				end
+			})
+		end
+	end,
 }
